8ecc82b25cef87a4dffbcda8da81813bbd84d169
Karam Barakat <kb.karam.barakat@gmail.com>
2025-05-19 15:28:43 -0700

    work on collection derive

--- claw-ql-macros/src/lib.rs
+++ claw-ql-macros/src/lib.rs
@@ -1,7 +1,21 @@
 use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
-use syn::{Item, parse};
+
 mod relation;
+mod collection_derive;
+
+#[proc_macro_derive(Collection)]
+#[proc_macro_error]
+pub fn collection(input: TokenStream) -> TokenStream {
+    let derive = match syn::parse::<syn::DeriveInput>(input) {
+        Ok(data) => data,
+        Err(err) => {
+            return err.to_compile_error().into();
+        }
+    };
+
+    collection_derive::main(derive).into()
+}
 
 // #[proc_macro]
 // #[proc_macro_error]
